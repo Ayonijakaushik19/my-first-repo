c20cd5510ea14ab3b9e72fcbd349efc74875ada3
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-16 00:11:39 +0530

    Create typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,28 @@
+#BINARY DATATYPE
+e=0b1010
+print(e,type(e))
+
+#HEXADECIMAL DATATYPE
+f=0XFFABC
+print(f,type(f))
+g=0xffabc 
+print(g,type(g))
+
+#boolean DATATYPE 
+i=True
+print(i,type(i))
+m=45>65
+print(m,type(m))
+
+#Type conversion/type casting
+#from boolean into integer
+a=int(True)
+print=(a,type(a))
+
+#integer to boolean
+d=bool(0)
+print(d,type(d))
+
+
+
+
