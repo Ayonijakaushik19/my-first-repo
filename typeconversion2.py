49782e068c9e4da17e3141a14efd35c9da9ba3fb
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-16 00:32:08 +0530

    Create typeconversion2.py

---
+++ typeconversion2.py
@@ -0,0 +1,33 @@
+#type conversion
+
+#into float
+a=float(34)
+print(a,type(a))
+b=float('8.7')
+print(b,type(b))
+
+#into string
+c=str(75)
+print(c,type(c))
+d=str(76.98888888888)
+print(d,type(d))
+
+#into boolean
+e=bool(50.7)
+print(e,type(e))
+f=bool(0)
+print(f,type(f))
+g=bool(67)
+print(g,type(g))
+
+#into hexadecimal
+h=hex(455)
+i=bin(766)
+print(h,type(h))
+print(i,type(i))
+#the data type will be string here
+
+
+
+
+
