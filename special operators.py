9e2839f8ef3420d50eb1a0ba59a69fce667c5512
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-16 02:08:45 +0530

    Create special operators.py

---
+++ special operators.py
@@ -0,0 +1,33 @@
+#repetition
+s='python is easy .'
+print(s*8)
+
+#length function
+print(len(s))
+
+#strip function
+g=" python is easy "
+print(g.strip())
+
+k=" python is easy"
+print(g.lstrip())
+
+#deleting a variable
+a=10
+del(a)
+#print(a)
+
+#shorthand operators
+count=0 
+count=count+1
+count=count+1
+count=count+1
+print(count)
+
+count1=0 
+count1+=1
+count1*=2
+print(count1)
+
+#special operator(in)
+print("pyth"in (s))
