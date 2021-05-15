15f1632d3d304b27dd92ed849b2e943499652194
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-16 01:45:54 +0530

    Create string slicing.py

---
+++ string slicing.py
@@ -0,0 +1,17 @@
+#create a string 
+s="python is so easy"
+print(s,type(s))
+
+#indexing
+print(s[8])
+
+# slicing
+print(s[5:]+s[5: :-1])
+print(s[-3:-1])
+print(s[16: :-1])
+
+#easy so is python
+l=s.split(" ")
+print(l)
+print(l[6: :-1])
+#doubt(why not 6,why 3)we are counting space but ayu is arguing
