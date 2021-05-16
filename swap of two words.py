1d03f94c9fa9e33358a14d530fd129cfe798490d
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-17 00:11:43 +0530

    Create swap of two words.py

---
+++ swap of two words.py
@@ -0,0 +1,19 @@
+word1="ayu"
+word2="khushu"
+print(word1+word2)
+word=word1
+word1=word2
+word2=word
+print(word2+word1)
+
+#word=ayu
+#word1=khushu
+#word2=ayu
+#ayukhushu
+# we create a another variable  'word' to save ayu's value so that it's value doesn't  get lost
+#then we assign word 1 to word 2 and then again pick ayu's value from word and assign it to word2 
+# as we have word=3 rd empty vessel
+#word 1=rice vessel
+#word 2=pulses vessel
+#and we have to exchange rice and pulses
+#so we put first rice to 3rd empty vessel and put pulses to rice vessel and then take rice from empty vessel and put it in pulses vessel
