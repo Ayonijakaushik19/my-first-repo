7e3142611232d89605a3e63d5c45f83b18c978b2
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-17 01:47:23 +0530

    Create if else block.py

---
+++ if else block.py
@@ -0,0 +1,28 @@
+bool_var=True
+x=1
+if bool_var:
+    x=x+1 
+    bool_var=not bool_var
+#bool_var=false  
+    if bool_var:#it's not saying that this bool_var (false) should be true,instead this whole if needs to be true.bool_var is the whole test. 
+        x=x+1 
+    else:
+        x=x-1
+print(x)  
+
+
+Bool_var=False
+y=8 
+if Bool_var:
+    y=y+5
+    Bool_var=not Bool_var
+#Bool_var=True
+    if Bool_var:
+        y=y-1
+    else:
+        y=y+1 
+print(y)        
+    
+    
+    
+    
