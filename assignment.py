e728db4c64b40c8cc71706613030536d3baa8a2f
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-17 00:49:21 +0530

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,18 @@
+x,y,z=5,6,7
+'''x=5
+y=6
+z=7'''
+
+#assignment is occuring here
+x=y=z
+
+#z goes into yand y goes to x 
+print(x==y==z)
+ #and eventually x,y and z will become equal
+ #the thing is that even if z's value goes to y,and ultimately y's value to x,even though the z's value won't be gone until n unless
+ #it's get replaced by a new value..
+print(x==y==z==7)
+
+
+ 
+
