c59541734fdef4c932196a7341a141941512880b
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-18 02:10:38 +0530

    Create conditional gpa.py

---
+++ conditional gpa.py
@@ -0,0 +1,11 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a**2==(b**2+c**2):
+    print("YES")
+elif b**2==(a**2+b**2):
+    print("YES")
+elif c**2==(a**2+b**2):
+    print("YES")
+else:
+    print("NO")
