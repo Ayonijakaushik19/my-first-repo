0b3743a16cdcb065412873906988f793134fbe98
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-18 02:26:20 +0530

    Create elif gpa2.py

---
+++ elif gpa2.py
@@ -0,0 +1,13 @@
+TIME=int(input())
+if TIME<0:
+    print("INVALID")
+elif TIME>=0 and TIME<=5:
+    print("NIGHT")
+elif TIME<=11 and TIME >=6:
+    print("MORNING")
+elif TIME>=12 and TIME<=17:
+    print("AFTERNOON")
+elif TIME>=18 and TIME<=23:
+    print('EVENING')
+else:
+    print("INVALID")
