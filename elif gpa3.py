be25c913ef0fb66d2b646d906bc03e5d70f7d078
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-18 02:55:27 +0530

    Create elif gpa3.py

---
+++ elif gpa3.py
@@ -0,0 +1,16 @@
+E1=int(input())
+E2=int(input())
+E3=int(input())
+E4=int(input())
+E5=int(input())
+if (E1+E2)%2==0 and (E1+E5)%2==0 or (E1+E4)%2==0 or (E1+E5)%2==0:
+    print("YES")
+elif (E2+E3)%2==0 or (E2+E4)%2==0 or (E2+E5)%2==0:
+    print("YES")
+elif (E3+E4)%2==0 or (E3+E5)%2==0:
+    print("YES")
+elif (E4+E5)%2==0:
+    print("YES")
+else:
+    print("NO")
+    
