a2150a5e66753f0d9b3fda9f1396b6c81966eda6
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-21 00:13:47 +0530

    Create Ifelse ppa2week 2.py

---
+++ Ifelse ppa2week 2.py
@@ -0,0 +1,19 @@
+x=float(input())
+y=float(input())
+if x>0 and y>0:
+    print("I")
+elif x<0 and y>0:
+    print("II")
+elif x<0 and y<0:
+    print("III")
+elif x>0 and y<0:
+    print("IV")
+elif x==0 and y!=0:
+    print("Y-axis")
+elif y==0 and x!=0:
+    print("X-axis")
+else:
+    print("Origin")
+    
+
+    
