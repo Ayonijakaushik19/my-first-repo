524e5eaadd8a102b75ce368ad5bf813555f378f8
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-22 22:51:59 +0530

    Update conditional gpa.py

--- conditional gpa.py
+++ conditional gpa.py
@@ -1,11 +1,15 @@
 a=int(input())
 b=int(input())
 c=int(input())
-if a**2==(b**2+c**2):
+if a<=0 or b<=0 or c<= 0:
+    print("NO")
+elif a**2==(b**2+c**2):
     print("YES")
 elif b**2==(a**2+b**2):
     print("YES")
 elif c**2==(a**2+b**2):
     print("YES")
+elif a==b==c:
+    print("No")
 else:
     print("NO")
