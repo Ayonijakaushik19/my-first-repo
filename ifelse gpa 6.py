8fbed43c29965e22002f38490f34cca81d111b66
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-22 22:49:21 +0530

    Update ifelse gpa 6.py

--- ifelse gpa 6.py
+++ ifelse gpa 6.py
@@ -6,7 +6,7 @@ if len(s)<8 or len(s)>32:
     print(False)
 elif  s[0] not in S1 and s[0] not in S2:
     print(False)
-elif  s.count("\"")>0 or s.count("/")>0 or s.count("=")>0 or s.count("'")>0 or s.count('"')>0 or s.count(' ')>0:
+elif  s.count("\"")>0 or s.count("/")>0 or s.count("=")>0 or s.count("'")>0 or s.count('"')>0 or s.count(' ')>0:   # to check special characters in my string which are not allowed
      print (False)
 else:
      print (True)
