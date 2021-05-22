53e37c6280f1979a3ab667e249a1c9826562aced
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-22 23:01:49 +0530

    Create Check Vowels.py

---
+++ Check Vowels.py
@@ -0,0 +1,12 @@
+str1 = input()
+
+if 'a' in str1 or 'A' in str1:
+    print('a',end ="")
+if 'e' in str1 or 'E' in str1:
+    print('e',end ="")
+if 'i' in str1 or 'I' in str1:
+    print('i',end ="")
+if 'o' in str1 or 'O' in str1:
+    print('o',end ="")
+if 'u' in str1 or 'U' in str1:
+    print('u',end ="")
