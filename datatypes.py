f458b62d0014d98c7392585f41ae31bc7870f9ee
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-23 22:52:41 +0530

    Update datatypes.py

--- datatypes.py
+++ datatypes.py
@@ -1,56 +1,19 @@
 
 first_name="David"
-
-
 print(first_name,type(first_name))
-
-
-
 last_name="andrews"
-
-
-
 print(last_name,type(last_name))
-
-
-
 son_of="Tom andrews"
-
-
-
 print(son_of,type(son_of))
-
-
-
 Date_of_birth="25 june 1990"
-
-
-
 print(Date_of_birth,type (Date_of_birth))
-
-
-
 Educational_qualification="I am a B.tech degree holder"
-
-
-
 print(Educational_qualification,type(Educational_qualification))
-
-
-
 Weight=65.6
-
-
-
 print(Weight,type(Weight))
-
-
-
-
-
-
-
 print(Weight>70,type(Weight>70))
+height = 5.4
+print(height,type(height))
 
 
 
