8cee37d0011c15126505748b4a548f896a7be2f1
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-24 23:26:38 +0530

    Update elif gpa 3(b).py

--- elif gpa 3(b).py
+++ elif gpa 3(b).py
@@ -3,11 +3,11 @@ E2=int(input())
 E3=int(input())
 E4=int(input())
 E5=int(input())
-if (E1+E2)%2==0 and (E1+E5)%2==0 :
+if (E1+E2)%2==0 and (E1+E5)%2==0:
     print("YES")
-elif (E2+E3)%2==0 and (E2+E1)%2==0 :
+elif (E2+E3)%2==0 and (E2+E1)%2==0:
     print("YES")
-elif (E3+E4)%2==0 and (E3+E2)%2==0 :
+elif (E3+E4)%2==0 and (E3+E2)%2==0:
     print("YES")
 elif (E4+E5)%2==0 and (E4+E3)%2==0:
     print("YES")
