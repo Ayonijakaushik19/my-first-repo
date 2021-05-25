049cae7d69465dc8b2f1fac78bf0c8e671b1b3e0
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-26 00:32:50 +0530

    Create for if else grpa 2 week 3.py

---
+++ for if else grpa 2 week 3.py
@@ -0,0 +1,17 @@
+n=int(input())
+s=[]
+if n<2:
+    print(n)
+else:
+    for i in range(2,n+1):
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            s.append(i)
+if len(s)==0:
+    print(n)
+else:
+    for x in s:
+        if n%x==0:
+            print(x)
