f9d7c04d6d932709de0ca1a08eea15669461b49e
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-27 22:22:30 +0530

    Update slicing.py

--- slicing.py
+++ slicing.py
@@ -9,4 +9,4 @@ print(s[::-b])
 print(s[c:0:-3])
 print(s[len(s):-d:-3])
 print(s[:e:-3])
-#[:e:-3]-,means starting from the reverse side(as it's -3) going upto ending index (e),which is  zero
+#[:e:-3]- means starting from the reverse side(as it's -3) going upto ending index (e),which is  zero
