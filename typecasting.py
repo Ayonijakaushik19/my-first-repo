ff2a907c7207f2c5fb52fabbef1b2f588f49657b
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-27 22:27:39 +0530

    Update typecasting.py

--- typecasting.py
+++ typecasting.py
@@ -1,14 +1,14 @@
-#BINARY DATATYPE
+#Binary DataType
 e=0b1010
 print(e,type(e))
 
-#HEXADECIMAL DATATYPE
+#HexDecimal DataType
 f=0XFFABC
 print(f,type(f))
 g=0xffabc 
 print(g,type(g))
 
-#boolean DATATYPE 
+#boolean DataType 
 i=True
 print(i,type(i))
 m=45>65
