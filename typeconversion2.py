01a42447b523bc6b18486813285dc2fe584077f5
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-27 22:24:07 +0530

    Update typeconversion2.py

--- typeconversion2.py
+++ typeconversion2.py
@@ -6,13 +6,13 @@ print(a,type(a))
 b=float('8.7')
 print(b,type(b))
 
-#into string
+# type conversion into string
 c=str(75)
 print(c,type(c))
 d=str(76.98888888888)
 print(d,type(d))
 
-#into boolean
+#type conversion into boolean
 e=bool(50.7)
 print(e,type(e))
 f=bool(0)
@@ -20,7 +20,7 @@ print(f,type(f))
 g=bool(67)
 print(g,type(g))
 
-#into hexadecimal
+#type conversion into hexadecimal
 h=hex(455)
 i=bin(766)
 print(h,type(h))
