eeb2a8cc5f043a7929df3bd751f01f9e32d173ff
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-30 22:24:01 +0530

    Create HCF of two numbers-2.py

---
+++ HCF of two numbers-2.py
@@ -0,0 +1,26 @@
+    x = int(input())
+    y = int(input())
+
+# Minimum of the two numbers
+    minimum = min(x, y)
+ 
+    # If both the numbers are divisible
+    # by the minimum of these two then
+    # the HCF is equal to the minimum
+    if (x % minimum == 0 and y % minimum == 0):
+        return minimum
+ 
+    # Highest number between 2 and minimum/2
+    # which can divide both the numbers
+    # is the required HCF
+    for i in range(minimum // 2, 1, -1):
+         
+        # If both the numbers are divisible by i
+        if (x % i == 0 and y % i == 0):
+            print(i)
+            break
+     else:
+      print(1)  # 1 divides every number
+ 
+   
+ 
