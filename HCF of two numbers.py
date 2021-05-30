58163faa8709b64aff80bcc9c490d179b01337d6
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-30 22:07:13 +0530

    Create HCF of two numbers.py

---
+++ HCF of two numbers.py
@@ -0,0 +1,17 @@
+# Function to return hcf of a and b
+def hcf(a,b):
+	
+	# Everything divides 0
+	if (b == 0):
+		return a
+	return hcf(b, a%b)
+
+# Driver program to test above function
+a = int(input())
+b = int(input())
+if(hcf(a, b)):
+	print('GCD of', a, 'and', b, 'is', hcf(a, b))
+else:
+	print('not found')
+
+
