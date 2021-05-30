eb4a978d01582fe280c3da5b40ef0d0de170ccbc
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-05-30 22:00:58 +0530

    Create Reverse of a Number2.py

---
+++ Reverse of a Number2.py
@@ -0,0 +1,15 @@
+    # converting number to string
+    num = int(input())
+    string = str(num)
+ 
+    # reversing the string
+    string = list(string)
+    string.reverse()
+    string = ''.join(string)
+ 
+    # converting string to integer
+    num = int(string)
+    
+    #print the number
+    print(num)
+ 
