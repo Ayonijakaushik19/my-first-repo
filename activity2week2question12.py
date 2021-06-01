ad5f6900d04e4a96579e9ab15fe1adcbac0b74e5
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-02 01:17:30 +0530

    Create activity2week2question12.py

---
+++ activity2week2question12.py
@@ -0,0 +1,20 @@
+x,y,z=int(input()) , int(input()) , int(input())
+
+if x+y==z:
+   print('good triplet')
+elif y+z==x:
+    print('good triplet')
+elif x+z==y:
+    print('good triplet')
+else:
+    print('bad triplet')
+ 
+ 
+#2nd way:
+ 
+    
+x,y,z=int(input()),int(input()),int(input())
+if x+y==z or y+z==x or x+z==y:
+    print('good triplet')
+else:
+    print('bad triplet')
