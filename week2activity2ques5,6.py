10142da813ed0ed411dc48de840a36604af07b9b
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-02 00:26:01 +0530

    Create week2activity2ques5,6.py

---
+++ week2activity2ques5,6.py
@@ -0,0 +1,19 @@
+#question5
+
+if True:
+   print('first')
+else:
+    print('second')
+    
+#second will never come..true is always true
+
+#question 6
+
+#left
+x= True
+if x==True:
+    print('works')
+#RIGHT
+x=True
+if x:
+    print('Works')
