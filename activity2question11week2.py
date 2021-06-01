90ccd08908e8bab7c20f66834584eace04839365
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-02 01:01:55 +0530

    Create activity2question11week2.py

---
+++ activity2question11week2.py
@@ -0,0 +1,31 @@
+n=int(input())
+lastdigit=n%10
+if lastdigit==0:
+    print('zero')
+elif lastdigit==1:
+    print('one')
+    
+elif lastdigit==2:
+    print('two')
+
+elif lastdigit==3:
+    print('three')
+    
+elif lastdigit==4:
+    print('four')
+    
+elif lastdigit==5:
+    print('five')
+    
+elif lastdigit==6:
+    print('six')
+    
+elif lastdigit==7:
+    print('seven')
+    
+elif lastdigit==8:
+    print('eight')
+    
+else:
+    print("nine")
+    
