84e6412a9cb3e42ae60c1f12b847345416929ea9
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-02 02:03:26 +0530

    Update ac2we2q17.py

--- ac2we2q17.py
+++ ac2we2q17.py
@@ -5,7 +5,7 @@ else:
     print('else')
     
 #if we pass empty string or zero in if statement then it treat as false    
-    
+#if we pass any string or any number except empty or zerro then it treat as true and if statement will be executed 
 x=54
 if x:
     print('if')
