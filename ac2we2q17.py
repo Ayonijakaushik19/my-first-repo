94939276cc8a6908212838c73703f310bf19fe2b
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-02 01:59:08 +0530

    Create ac2we2q17.py

---
+++ ac2we2q17.py
@@ -0,0 +1,13 @@
+empty=''
+if empty:
+    print('if')
+else:
+    print('else')
+    
+#if we pass empty string or zero in if statement then it treat as false    
+    
+x=54
+if x:
+    print('if')
+else:
+    print('else')
