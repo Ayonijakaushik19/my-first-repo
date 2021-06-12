d94da7bfbcf87ba04a38ebf84d616242062756e6
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-12 22:53:56 +0530

    Update HCF of two numbers.py

--- HCF of two numbers.py
+++ HCF of two numbers.py
@@ -6,11 +6,13 @@ def hcf(a,b):
 	return hcf(b, a%b)
 
 # Driver program to test above function
+
 a = int(input())
 b = int(input())
 if(hcf(a, b)):
 	print('GCD of', a, 'and', b, 'is', hcf(a, b))
 else:
 	print('not found')
+	
 
 
