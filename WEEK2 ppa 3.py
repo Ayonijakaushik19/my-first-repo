7b6e32b0c22a3164142ce5598673eccd780ae2e9
Ayonijakaushik19 <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-14 00:04:35 +0530

    Create WEEK2 ppa 3.py

---
+++ WEEK2 ppa 3.py
@@ -0,0 +1,25 @@
+x1=int(input())
+y1=int(input())
+x2=int(input())
+y2=int(input())
+x3=int(input())
+if x1==x2:
+    print("Vertical Line")
+elif y1==y2:
+    print("Horizontal Line")
+else:
+    y3=((x3-x1)*(y2-y1))/(x2-x1)
+    y3+=y1
+    print(y3)
+    if y3>0:
+        print("Positive Slope")
+    else:
+        print("Negative Slope")
+        
+
+    
+
+    
+
+    
+
