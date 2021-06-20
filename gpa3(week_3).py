750b743be7c6917d4558df7aced6b5232baf5430
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-21 01:24:12 +0530

    Create gpa3(week_3).py

---
+++ gpa3(week_3).py
@@ -0,0 +1,5 @@
+E1,E2,E3,E4,E5=int(input()),int(input()),int(input()),int(input()),int(input())
+if (E1+E2)%2==0 or (E2+E3)%2==0 or (E3+E4)%2==0 or (E4+E5)%2==0 or (E5+E1)%2==0:
+    print("YES")
+else:
+    print("NO")
