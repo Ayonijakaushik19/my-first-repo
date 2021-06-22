a3c6ed8d88e1d55ddc34863406cca8975948fdbf
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-22 23:36:19 +0530

    Update for loop week 3 grpa1.py

--- for loop week 3 grpa1.py
+++ for loop week 3 grpa1.py
@@ -2,8 +2,7 @@ n=int(input())
 sum=0
 for i in range(1,n+1):
     k=i*(i+1)
-    
- #we are taking sum of first n natural numbers but
+#we are taking sum of first n natural numbers but
 #one important thing that was twist in this question is that we are retaining the previous sum value into the term
 #as for example if n=3
          #1+(1+2)+(1+2+3)
