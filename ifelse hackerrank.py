59f01900b6bd60cb5085b11fdfd5f4a9d3bf4882
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-23 11:17:30 +0530

    Update ifelse hackerrank.py

--- ifelse hackerrank.py
+++ ifelse hackerrank.py
@@ -2,11 +2,11 @@ n=int(input())
 if n%2!=0:
     print("Weird")
 elif n%2==0:
-    if n==range(2,6):
+    if n in range(2,6):
         print('Not Weird')
-    elif n==range(6,21):
+    elif n in range(6,21):
         print("Weird")
-    elif n>20:
+    else:
         print("Not Weird")
         
     
