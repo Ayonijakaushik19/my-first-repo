5c12636d4760d32ddc420a51ba04a340fb69740e
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-23 11:16:39 +0530

    Create if else.py

---
+++ if else.py
@@ -0,0 +1,11 @@
+n=int(input())
+if n%2!=0:
+    print("Weird")
+elif n%2==0:
+    if n in range(2,6):
+        print('Not Weird')
+    elif n in range(6,21):
+        print("Weird")
+    elif n>20:
+        print("Not Weird")
+        
