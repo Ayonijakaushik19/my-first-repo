022ad696febd232c56f1eae9ba5e104c6204601f
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-23 20:43:16 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+lst=[]
+n=int(input())
+lst.insert(0,6)
+lst.insert(1,7)
+lst.insert(2,8)
+lst.insert(1,9)
+print(lst)
+lst.remove(9)
+lst.append(9)
+lst.sort()
+print(lst)
+lst.pop()
+lst.sort(reverse=True)
+print(lst)
+#pop is an inbuilt function
