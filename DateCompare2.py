6f54fc0b1e602a25a831d885bffbb3efd4d11c47
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-26 23:26:07 +0530

    Update DateCompare2.py

--- DateCompare2.py
+++ DateCompare2.py
@@ -10,6 +10,7 @@ b1 = date(y1, m1, d1)
 # Input for second date
 d2, m2, y2 = [int(x) for x in input("Enter second"
 		" person's date(DD/MM/YYYY) : ").split('/')]
+
 b2 = date(y2, m2, d2)
 # Check the dates
 if b1 == b2:
