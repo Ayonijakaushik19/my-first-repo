e41662325911f6fb5f137a82153741f0506d9155
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-29 23:05:25 +0530

    Create forpattern.py

---
+++ forpattern.py
@@ -0,0 +1,18 @@
+#This is the example of print simple pyramid pattern
+
+
+n=int(input())
+#outer loop to handle number of rows
+
+for i in range(0,n):
+    
+    #inner loop to handle number of columns
+    #values is changing according to outer loop
+    for j in range(0,i+1):
+        
+        #printing stars
+        print("*",end="")
+        
+    #ending line after each row
+    print()
+
