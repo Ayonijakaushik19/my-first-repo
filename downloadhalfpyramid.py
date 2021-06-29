22d31c49d3f0276b1fa77ede1c795308ecce707d
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-06-29 23:43:09 +0530

    Create downloadhalfpyramid.py

---
+++ downloadhalfpyramid.py
@@ -0,0 +1,11 @@
+#printin downward half pyramid
+
+n = int(input())  
+  
+# the outer loop is executing in reversed order  
+for i in range(n, 0, -1):    
+    for j in range(0, i-1 ): 
+       # print(j)
+        
+        print("*", end=' ')  
+    print(" ")  
