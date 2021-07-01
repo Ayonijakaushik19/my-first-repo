62d268444c0c5dfeba4c003f8e52bb2a54ed5379
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-02 01:22:05 +0530

    Create pattern98.py

---
+++ pattern98.py
@@ -0,0 +1,12 @@
+n=int(input())
+
+for i in range(0,n):
+    for k in range(n-i,0,-1):
+        print(" ",end='')
+        
+    for j in range(0,i+1):
+        print(n-i,end='')
+    print()
+        
+#notice the pattern dependancy,,and the values dependency 
+        
