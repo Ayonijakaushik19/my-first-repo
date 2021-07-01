e6ce43a40cbf745df7d8deadcea87d76a40bbba6
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-02 00:49:01 +0530

    Create squarepattern.py

---
+++ squarepattern.py
@@ -0,0 +1,13 @@
+#pattern 1
+
+#square pattern with equal number of rows and columns
+
+rows=int(input())
+
+for i in range(0,rows+1):
+    for j in range(0,rows+1):
+        print('*',end='')
+    print()
+
+
+
