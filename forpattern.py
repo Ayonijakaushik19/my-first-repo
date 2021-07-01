7eaa48cc2090736ae35650503ccc13b5a1903621
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-01 22:47:07 +0530

    Update forpattern.py

--- forpattern.py
+++ forpattern.py
@@ -8,6 +8,7 @@ for i in range(0,n):
     
     #inner loop to handle number of columns
     #values is changing according to outer loop
+    
     for j in range(0,i+1):
         
         #printing stars
