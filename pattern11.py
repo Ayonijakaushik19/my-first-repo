1564c935398a69278344b4008604885fa04ad93c
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-03 02:41:07 +0530

    Create pattern11.py

---
+++ pattern11.py
@@ -0,0 +1,18 @@
+n=int(input())
+
+a=1
+for i in range(1,n):
+    a=1
+    for j in range(1,n+1):
+        if j%2==0:
+            
+            print(a,end="")
+            a=a+1
+        else:
+            print(i,end="")
+
+        
+    print()
+        
+         
+    
