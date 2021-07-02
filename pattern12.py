17d08e398220d9cf6fa5f7454b14e7836ef31c1f
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-03 03:27:46 +0530

    Create pattern12.py

---
+++ pattern12.py
@@ -0,0 +1,17 @@
+n=int(input())
+
+a=1
+b=1
+for i in range(1,n+1):
+    
+    
+    for j in range(1,n+1):
+        
+        print(a,end=" ")
+        a=a+5
+    b=b+1
+    a=b
+    print()
+   
+
+        
