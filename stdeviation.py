6a79c847b41546a4e7284bdf6cd4123dcf44b586
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-13 23:56:46 +0530

    Update stdeviation.py

--- stdeviation.py
+++ stdeviation.py
@@ -24,7 +24,7 @@ for y in lst:
 standard_deviation=summation/(length-1)
 
 #we can use **0.5 also as power in the process of taking square root in place of math function
-print(math.sqrt(standard_deviation))
+#print(math.sqrt(standard_deviation))
 print(round(math.sqrt(standard_deviation),2))
 
 
