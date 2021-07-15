5e246a3b62578b2174510aad3676e5cdbe184ee9
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-15 23:51:48 +0530

    Update pattern96.py

--- pattern96.py
+++ pattern96.py
@@ -1,8 +1,7 @@
 n=int(input())
 for i in range(0,n):
     for k in range(n-i,0,-1):
-        print(" ",end='')
-        
+        print(" ",end='')  
     for j in range(0,i+1):
         print(i+1,end='')
     print()
