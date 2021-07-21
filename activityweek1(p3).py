5d25a919a7e4f19cee2654aa9b8f581800a50d64
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-21 23:05:16 +0530

    Create activityweek1(p3).py

---
+++ activityweek1(p3).py
@@ -0,0 +1,9 @@
+#question number 23
+
+a=int(input())
+b=int(input())
+print(0<a<b<10)
+print(b-a==1)
+
+# for both true and true there can be eight choices-(1,3),(2,3),(3,4,(4,5),(5,6),(6,7),(7,8),(8,9)
+#for the values of a and b respectively
