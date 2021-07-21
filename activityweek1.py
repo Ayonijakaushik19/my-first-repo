7dfd01f9fb154260ec7d22a4433d8eab29f568f1
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-21 22:24:24 +0530

    Create activityweek1.py

---
+++ activityweek1.py
@@ -0,0 +1,21 @@
+#question number 14
+
+"""n=7**100
+s=str(n)
+print(len(s))"""
+
+
+#question number 25
+
+
+"""n=int(input())
+sq_num=int(n**0.5)
+print(sq_num**2==n)"""
+
+# alternative way of question number 25
+import math
+m=int(input())
+
+q=int(math.sqrt(m))
+
+print(q**2==m)
