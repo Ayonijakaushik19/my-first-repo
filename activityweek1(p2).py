9b0083d769d098ff7f1fe39488ff985d539a48bb
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-21 22:55:18 +0530

    Create activityweek1(p2).py

---
+++ activityweek1(p2).py
@@ -0,0 +1,23 @@
+#question number 24
+
+"""n=int(input())
+q=str(n)
+sum=int(q[0])+int(q[1])
+print(sum)"""
+
+
+"""n=int(input())
+
+q=n%10
+n=n//10
+
+print(q+n)"""
+
+#alternative way of question number 24
+
+n=int(input())
+q=n%100
+thirddigit=q%10
+q=q//10
+n=n//100
+print(thirddigit+q+n)
