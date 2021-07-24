4fcf64a4dcf3afdbb30159800a8d869317f995fe
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-24 14:30:17 +0530

    Create activityweek2.py

---
+++ activityweek2/activityweek2.py
@@ -0,0 +1,45 @@
+n=3**100
+n=str(n)
+print(n)
+cnt=n.count("3")
+print(cnt)
+
+if n.count("0")==0 and n.count("1")==0 and n.count("2")==0 and n.count("3")==0 and n.count("4")==0 and n.count("5")==0 and n.count("6")==0 and n.count("7")==0  and n.count("8")==0  and n.count("9")==0:
+    print("False")
+else:
+    print("True")
+    
+if n.count("0")==0:
+    print("0")
+if n.count("1")==0:
+    print("1")
+if n.count("2")==0:
+    print("2")
+
+if n.count("3")==0:
+    print("3")
+    
+if n.count("4")==0:
+    print("4")
+    
+if n.count("5")==0:
+    print("5")
+    
+if n.count("6")==0:
+    print("6")
+    
+if n.count("7")==0:
+    print("7")
+    
+if n.count("8")==0:
+    
+    print("8")
+if n.count("9")==0:
+    
+    print("9")
+    
+     
+    
+    
+    
+    
