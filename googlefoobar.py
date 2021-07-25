e4e32321fd473938c78c213d5053e27a87b17204
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-25 11:08:31 +0530

    Create googlefoobar.py

---
+++ googlefoobar.py
@@ -0,0 +1,18 @@
+n=1
+
+while n<=50:
+    if n%3 ==0 and n%5==0:
+        
+        print("foobar")
+        
+    elif n%3==0:
+        
+        print("foo")
+        
+    elif n%5==0:
+        
+        print("bar")
+        
+    
+    n+=1
+    print(n
