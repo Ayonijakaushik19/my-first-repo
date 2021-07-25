fbe270d43a31a9949ab2b47144f52d23490f5f01
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-25 13:01:43 +0530

    Create activity3week3q.no.4.py

---
+++ activity3week3q.no.4.py
@@ -0,0 +1,42 @@
+ -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 25 12:31:23 2021
+
+@author: ayoni
+"""
+
+import sympy
+
+n=int(input())
+
+i=1
+count=0
+
+while i<=n:
+    if sympy.isprime(i):
+        count+=1
+        
+        print(i)
+    i+=1
+print(count)
+        
+# for prime numbers
+
+q=int(input())
+m=1
+count1=0
+
+while m<=q:
+    if sympy.isprime(m):
+        m+=1
+        continue
+    else:
+        count1+=1
+        print(m)
+    m+=1
+    
+print(count1)    
+    
+#for composite numbers        
+        
+        
