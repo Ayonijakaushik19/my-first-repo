80a756ccbf5406ce7b21d045c6f27d4bc16a857e
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-25 12:25:11 +0530

    Create activity3week3.ques3.py

---
+++ activity3week3.ques3.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 25 11:42:56 2021
+
+@author: ayoni
+"""
+
+import math
+
+n=int(input())
+i=1
+sum=0
+while i<=n:
+    
+    
+    f= math.factorial(i)
+    
+    print(f)
+    i+=1
+    sum+=f
+    
+    
+average=sum/n   
+print(average) 
+    
+"""to find the average of the sum of the factorial of 1 to n numbers   
+    
+    
+    
+    
+    
+    
+    
