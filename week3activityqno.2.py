f15f45bef1fcdad0fdc08d6814598743b1d67643
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-25 11:42:15 +0530

    Create week3activityqno.2.py

---
+++ week3activityqno.2.py
@@ -0,0 +1,37 @@
+"""
+Created on Sun Jul 25 11:18:31 2021
+
+@author: ayoni
+"""
+
+n=0
+
+while 0<=n<=9:
+    n+=1
+    print(n)
+ #for 10 numbers
+   
+m=0
+
+while m<=19:
+    if m%2!=0:
+        print(m)
+        
+        m+=1
+    else:
+        m+=1
+        
+#for 10 even numbers
+        
+q=20
+
+while q>=1:
+    if q%2==0:
+        print(q)
+        q-=1
+        
+    else:
+        q-=1
+#for 10 even numbers in reverse order  
+    
+    
