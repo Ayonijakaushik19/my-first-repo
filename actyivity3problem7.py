706e9197dcbdc3a403d83da77dc8db86357a801b
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-26 23:09:32 +0530

    Create actyivity3problem7.py

---
+++ actyivity3problem7.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 22:53:35 2021
+
+@author: ayonija kaushik
+"""
+
+n=int(input())
+i=1
+while n>=i:
+    print("Current number is :",i," and the cube is",i**3 )
+    i+=1
