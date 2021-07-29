af015ddf5e4f8303eddc2c776d8f4990f1e21b1f
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-29 12:12:31 +0530

    Create swapingletters.py

---
+++ swapingletters.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 29 10:34:58 2021
+
+@author: ayonija
+"""
+
+string=input()
+a=len(string)
+
+modified_string=""
+
+for i in range(a):
+    if string[i].isupper():
+        modified_string+=string[i].lower()
+    elif string[i].islower():
+        modified_string+=string[i].upper()
+    else:
+        modified_string+=string[i]
+print(modified_string)
+    
