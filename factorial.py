8352df7250c1fb5e91fdb3c7f618e50f266df7ec
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-08-06 11:31:34 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,18 @@
+#factorial of a number
+n=int(input())
+fac=1
+for i in range(1,n+1):
+    fac=fac*i
+print(fac)
+    
+    
+# another method    
+n=int(input())
+fact=1
+if n<0:
+    print("Not defined")
+else:
+    while (n>0):
+        fact=fact*n
+        n=n-1
+    print(fact)    
