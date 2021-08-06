835e35019307cfec84c281d70fc6e1fcfd339d7f
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-08-06 13:07:12 +0530

    Create hcf.py

---
+++ hcf.py
@@ -0,0 +1,15 @@
+#find HCF
+n1=int(input())
+n2=int(input())
+mini=0
+if(n1==n2):
+    print(n1,"HCF is",n1)
+else:
+    if n1<n2:
+        mini=n1
+    if n2<n1:
+        mini=n2
+for i in range(mini,1,-1):
+    if n1%i==0 and n2%i==0:
+        print(i,"HCF is",i)
+        break
