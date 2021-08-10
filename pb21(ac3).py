ce0e1097d2fa20ad3ecf0d5d93467e8ed72e6f66
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-08-10 13:35:36 +0530

    Create pb21(ac3).py

---
+++ pb21(ac3).py
@@ -0,0 +1,13 @@
+#product of digits of a number 
+
+n=abs(int(input()))
+product=1
+if n==0:
+    print("0")
+else:
+    while (n>0):
+        
+        digits=n%10
+        n=n//10
+        product*=digits
+    print(product)
