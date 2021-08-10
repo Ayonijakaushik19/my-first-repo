7f98d850752a1020e6661a67b63b4d30bb2187e3
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-08-10 16:07:45 +0530

    Create ac3.28.py

---
+++ ac3.28.py
@@ -0,0 +1,23 @@
+n1=int(input())
+n2=int(input())
+# n1 and n2 are same if they have equal number of digits in them
+count1=0
+count2=0
+while n1>0:
+    count1+=1
+    n1=n1//10
+#we can't alter range in for loop but here range is changing during change in n2,so we don't use for loop here
+#n1 and n2 chota ho rha hai
+while n2>0:
+    count2+=1
+    n2=n2//10
+
+if count1==count2:
+    print("Same number")
+else:
+    print("Not same number")
+    
+    
+
+    
+#But here it fails when one number is negative and another numebr is positive
