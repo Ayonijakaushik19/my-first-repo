1d64798fae769df053de471257b6504724b67c20
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-08-12 22:05:58 +0530

    Update for loop week 3 grpa1.py

--- for loop week 3 grpa1.py
+++ for loop week 3 grpa1.py
@@ -6,7 +6,6 @@ for i in range(1,n+1):
 #one important thing that was twist in this question is that we are retaining the previous sum value into the term
 #as for example if n=3
          #1+(1+2)+(1+2+3)
-    
     k=k/2
     sum+=k
 print(int(sum))
