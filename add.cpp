b611df1584f168f7951a89306de644b3abdb55f4
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-08-20 10:18:59 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a,b,c;
+    cout<<"Enter two numbers"<<" ";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Sum is"<<" "<<c;
+    return 0;
+    
+}
+
+	
+
