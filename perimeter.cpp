cf1b55cecd7983cf3284fc552977ac4bd6d71c3a
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-08-20 10:40:08 +0530

    Create perimeter.cpp

---
+++ perimeter.cpp
@@ -0,0 +1,12 @@
+//Perimeter of a rectangle
+#include <iostream>
+using namespace std;
+int main()
+{
+    float l,b,perimeter;
+    cin>>l>>b;
+    perimeter=2*(l+b);
+    cout<<perimeter;
+    return 0;
+
+}
