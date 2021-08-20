3a006df9b1ed5939a1d9436ea65b0f060ae11e85
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-08-20 10:19:41 +0530

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,18 @@
+//TO ADD TWO NUMBERS
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a,b,c;
+    cout<<"Enter two numbers"<<" ";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Sum is"<<" "<<c;
+    return 0;
+    
+}
+
+	
+
