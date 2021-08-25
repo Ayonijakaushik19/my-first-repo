ef7829a1830ab4d0829e0be59fc851ffc9501815
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-08-25 23:35:53 +0530

    Update Week4_ppa.py

--- Week4_ppa.py
+++ Week4_ppa.py
@@ -4,7 +4,6 @@ print(x)
 while x in st:
     st=st[:-1]  // string ko har bar reverse karke apne aap ko assign kiya gya jaise ayush ka hsuya ki usne first time 
     print(x,end="")
-    
-    
+        
 # st ka length - index of X in st     
 # mere case m 5 tha X index 2 tha to mera X character 3 baar print hua 
